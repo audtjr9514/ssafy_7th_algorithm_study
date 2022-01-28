d3be0b56cc7aefb04b4340cc741f5e9ed5452c58
김명석 <37326982+audtjr9514@users.noreply.github.com>
2022-01-29 00:02:20 +0900

    22.01.29 1주차 BOJ_10828

---
+++ 명석/src/Week1/BOJ_10828.java
@@ -0,0 +1,45 @@
+package Week1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            if (str.equals("push"))
+                stack.push(Integer.parseInt(st.nextToken()));
+            else if (str.equals("pop"))
+                if (stack.empty())
+                    bw.write(-1 + "\n");
+                else bw.write(stack.pop() + "\n");
+            else if (str.equals("size"))
+                bw.write(stack.size() + "\n");
+            else if (str.equals("empty"))
+                if (stack.empty())
+                    bw.write(1 + "\n");
+                else bw.write(0 + "\n");
+            else if (str.equals("top"))
+                if (stack.empty())
+                    bw.write(-1 + "\n");
+                else
+                    bw.write(stack.peek() + "\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
